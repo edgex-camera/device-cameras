2b855ae21616a5a46086984d8dc86eb88b104486
250lth <gang250lth@gmail.com>
2020-02-04 17:10:57 +0800

    init repo

---
+++ cmd/device-service/main.go
@@ -0,0 +1,29 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+//
+// Copyright (C) 2017-2018 Canonical Ltd
+// Copyright (C) 2018-2019 IOTech Ltd
+//
+// SPDX-License-Identifier: Apache-2.0
+
+// This package provides a device service of cameras.
+package main
+
+import (
+	"github.com/edgexfoundry/device-sdk-go"
+	"github.com/edgexfoundry/device-sdk-go/pkg/jxstartup"
+	"gitlab.jiangxingai.com/applications/edgex/device-service/device-cameras/internal/driver"
+	"gitlab.jiangxingai.com/applications/edgex/device-service/device-cameras/internal/restful"
+)
+
+const (
+	serviceName = "device-cameras"
+	staticPath  = "/app/frontend"
+)
+
+func main() {
+	d := driver.Driver{}
+	err := jxstartup.StartServiceWithHandler(serviceName, device.Version, &d, restful.InitRestRoutes, staticPath)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ internal/restful/restful.go
@@ -0,0 +1,34 @@
+package restful
+
+import (
+	"net/http"
+
+	"github.com/edgexfoundry/device-sdk-go"
+	"github.com/edgexfoundry/go-mod-core-contracts/clients/logger"
+	"github.com/gorilla/mux"
+)
+
+type handler struct {
+	*mux.Router
+	lc logger.LoggingClient
+}
+
+const APIv1Prefix = "/api/v1"
+
+func InitRestRoutes(lc logger.LoggingClient) http.Handler {
+	h := &handler{
+		Router: mux.NewRouter(),
+		lc:     lc,
+	}
+	r := h.Router.PathPrefix(APIv1Prefix).Subrouter()
+	r.HandleFunc("/template_output", h.getTemplateOutput).Methods(http.MethodGet)
+	return h
+}
+
+func (h *handler) getTemplateOutput(w http.ResponseWriter, r *http.Request) {
+	out := device.DriverConfigs()["TemplateOutput"]
+	_, err := w.Write([]byte(out))
+	if err != nil {
+		h.lc.Error(err.Error())
+	}
+}
