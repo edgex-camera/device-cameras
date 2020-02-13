a3967d5a8d980af99b6aad18b1b2843ff7478bff
250lth <gang250lth@gmail.com>
2020-02-14 01:02:47 +0800

    add image maintainer

--- internal/lib/camera/camera.go
+++ internal/lib/camera/camera.go
@@ -16,6 +16,7 @@ type RawCamera struct {
 	CameraConfig    CameraConfig
 	processes       []process.Process
 	videoMaintainer *videoMaintainer
+	imageMaintainer *imageMaintainer
 	enabled         bool
 }
 
@@ -55,6 +56,15 @@ func (c *RawCamera) Enable() {
 		}
 		c.videoMaintainer = videoMaintainer
 	}
+
+	if c.CameraConfig.CaptureConfig.Enabled && c.CameraConfig.CaptureConfig.Storage {
+		imageMaintainer, err := newImageMaintainer(c.Lc, c.CameraConfig.CaptureConfig.Path, c.CameraConfig.CaptureConfig.Seconds, c.CameraConfig.CaptureConfig.Number)
+		if err != nil {
+			c.Lc.Error(fmt.Sprintf("image maintainer init failed: %v", err.Error()))
+		}
+		c.imageMaintainer = imageMaintainer
+		c.imageMaintainer.start()
+	}
 }
 
 func (c *RawCamera) Disable(wait bool) {
@@ -79,6 +89,10 @@ func (c *RawCamera) Disable(wait bool) {
 	if c.videoMaintainer != nil {
 		c.videoMaintainer.stop()
 		c.videoMaintainer = nil
+	}
+	if c.imageMaintainer != nil {
+		c.imageMaintainer.stop()
+		c.imageMaintainer = nil
 	}
 }
 
@@ -112,4 +126,8 @@ func (c *RawCamera) GetVideoPaths() []string {
 
 func (c *RawCamera) GetStreamAddr() string {
 	return c.CameraConfig.StreamConfig.Address
+}
+
+func (c *RawCamera) GetImagePaths() []string {
+	return c.imageMaintainer.getFileList()
 }
