ecc805561fcadf9c0817dc58577604921e8c8d62
JK-97 <benjamin-dream@outlook.com>
2020-02-14 19:00:06 +0800

    managae restful api

--- internal/restful/resource.go
+++ internal/restful/resource.go
@@ -10,19 +10,19 @@ func appendCameraResourcesRoute(r *mux.Router, h *handler) {
 	prefix := "/source"
 	subRouter := r.PathPrefix(prefix).Subrouter()
 
-	subRouter.Path("/capture_path").HandlerFunc(h.getCapturePath).Methods(http.MethodGet)
-	subRouter.Path("/stream_addr").HandlerFunc(h.getStreamAddr).Methods(http.MethodGet)
+	// subRouter.Path("/capture_path").HandlerFunc(h.getCapturePath).Methods(http.MethodGet)
+	// subRouter.Path("/stream_addr").HandlerFunc(h.getStreamAddr).Methods(http.MethodGet)
 
 	subRouter.Path("/video_paths").HandlerFunc(h.getVideoPaths).Methods(http.MethodGet)
 	subRouter.Path("/images").HandlerFunc(h.getImageURls).Methods(http.MethodGet)
 	subRouter.Path("/vidoes").HandlerFunc(h.getImageURls).Methods(http.MethodGet)
 }
 
-//获取截图路径
-func (h *handler) getCapturePath(w http.ResponseWriter, r *http.Request) {}
+// //获取截图路径
+// func (h *handler) getCapturePath(w http.ResponseWriter, r *http.Request) {}
 
-//获取视频流地址
-func (h *handler) getStreamAddr(w http.ResponseWriter, r *http.Request) {}
+// //获取视频流地址
+// func (h *handler) getStreamAddr(w http.ResponseWriter, r *http.Request) {}
 
 //获取视频存储路径
 func (h *handler) getVideoPaths(w http.ResponseWriter, r *http.Request) {}
