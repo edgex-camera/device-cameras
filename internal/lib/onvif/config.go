bff5c5f04f052cc18cb3ce350f27de3bced773e9
JK-97 <benjamin-dream@outlook.com>
2020-02-25 17:55:25 +0800

    fix consul config reading

--- internal/lib/onvif/config.go
+++ internal/lib/onvif/config.go
@@ -3,17 +3,16 @@ package onvif
 import (
 	"encoding/json"
 
-	"github.com/edgexfoundry/device-sdk-go"
 	"github.com/edgexfoundry/device-sdk-go/pkg/jxstartup"
 	jsonpatch "gopkg.in/evanphx/json-patch.v4"
 )
 
-func (c *OnvifCamera) MergeConfig() error {
+func (c *OnvifCamera) MergeConfig(conf map[string]string) error {
 	oldConf, err := json.Marshal(c.OnvifConfig)
 	if err != nil {
 		return err
 	}
-	configPatch, _ := device.DriverConfigs()[c.Name+".onvif.config"]
+	configPatch := conf[c.Name+".onvif.config"]
 	newConf, err := jsonpatch.MergePatch(oldConf, []byte(configPatch))
 	if err != nil {
 		return err
