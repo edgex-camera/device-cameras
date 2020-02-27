65fa3d9f62d4fe732dcdac8d298a42e488c72a84
JK-97 <benjamin-dream@outlook.com>
2020-02-27 16:34:32 +0800

    images and videos url

--- internal/restful/resource.go
+++ internal/restful/resource.go
@@ -2,6 +2,7 @@ package restful
 
 import (
 	"net/http"
+	"path"
 
 	"github.com/gorilla/mux"
 	"gitlab.jiangxingai.com/applications/edgex/device-service/device-cameras/internal/driver"
@@ -22,13 +23,28 @@ func appendCameraResourcesRoute(r *mux.Router, h *handler) {
 func (h *handler) getVideoPaths(w http.ResponseWriter, r *http.Request) {
 	type responce struct {
 		VideoPaths []string `json:"videopaths"`
+		Total      int      `json:"total"`
 	}
+
+	CameraConfigure, err := getCameraConfigJson(getCameraName(r))
+	if err != nil {
+		h.respFailed(err, w)
+		return
+	}
+
+	from, until := h.getTimestampInfo(r)
+
 	h.DoResp(
 		driver.CurrentDriver.JDevices[getCameraName(r)].Camera,
 		w,
 		func(c jdevice.Camera) interface{} {
+			pageInfo, total, err := h.timeFiliter(from, until, c.GetVideoPaths(), CameraConfigure.VideoConfig.Path, nil)
+			if err != nil {
+				return err
+			}
 			return responce{
-				VideoPaths: c.GetVideoPaths(),
+				VideoPaths: pageInfo,
+				Total:      total,
 			}
 		})
 
@@ -40,19 +56,23 @@ func (h *handler) getImageURls(w http.ResponseWriter, r *http.Request) {
 		ImageURLs []string `json:"imageurls"`
 		Total     int      `json:"total"`
 	}
+
 	offset, limit := h.getPageInfo(r)
 	h.DoResp(
 		driver.CurrentDriver.JDevices[getCameraName(r)].Camera,
 		w,
 		func(c jdevice.Camera) interface{} {
-
-			pageInfo, err := h.currentPage(offset, limit, c.GetVideoPaths())
+			names, total, err := h.listFilter(offset, limit, c.GetImagePaths(), func(data string) string {
+				_, name := path.Split(data)
+				return name
+			})
 			if err != nil {
 				return err
 			}
+			pageInfo := h.GenUrlByName(names, "images", getCameraName(r))
 			return responce{
 				ImageURLs: pageInfo,
-				Total:     len(c.GetImagePaths()),
+				Total:     total,
 			}
 		})
 }
@@ -63,19 +83,34 @@ func (h *handler) getVideoURLs(w http.ResponseWriter, r *http.Request) {
 		VideoURLs []string `json:"videourls"`
 		Total     int      `json:"total"`
 	}
-	offset, limit := h.getPageInfo(r)
+
+	CameraConfigure, err := getCameraConfigJson(getCameraName(r))
+	if err != nil {
+		h.respFailed(err, w)
+		return
+	}
+
+	from, until := h.getTimestampInfo(r)
 
 	h.DoResp(
 		driver.CurrentDriver.JDevices[getCameraName(r)].Camera,
 		w,
 		func(c jdevice.Camera) interface{} {
-			pageInfo, err := h.currentPage(offset, limit, c.GetVideoPaths())
+			names, total, err := h.timeFiliter(from, until,
+				c.GetVideoPaths(),
+				CameraConfigure.VideoConfig.Path,
+				func(data string) string {
+					_, name := path.Split(data)
+					return name
+				})
+
 			if err != nil {
 				return err
 			}
+			pageInfo := h.GenUrlByName(names, "videos", getCameraName(r))
 			return responce{
 				VideoURLs: pageInfo,
-				Total:     len(c.GetVideoPaths()),
+				Total:     total,
 			}
 		})
 
